5b536c7e88c47c1a5ffd683582628d5ebff0346f
josephx64 <josephx64@gmail.com>
2015-07-06 10:44:39 -0500

    Primer commit

---
+++ myAssets/js/miScript.js
@@ -0,0 +1,17 @@
+//miScript.js
+
+var numero =1;
+
+while(numero <=10){
+	console.log(numero);
+	numero++;
+}
+
+var trainNumber = 1;
+var trainOperational = 8;
+
+while (trainNumber <= trainOperational)
+{
+	console.log("El tren # "+trainNumber+" esta corriendo");
+	trainNumber++;
+}
